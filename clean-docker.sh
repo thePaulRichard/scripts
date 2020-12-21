c028905453a3f55353f20dd810075a5268926151
Paulo Paim <paulopaim@outlook.com>
2020-12-21 12:16:21 -0300

    Add files via upload

---
+++ clean-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+for i in $(docker ps -aq)
+do
+	echo $(docker stop $i)
+done
+
+for i in $(docker ps -aq)
+do
+	echo $(docker rm $i)
+done
+
+for i in $(docker images -aq)
+do
+	echo $(docker rmi $i)
+done
+
+for i in $(docker network ls --filter type=custom -q)
+do
+	echo $(docker network rm $i)
+done
